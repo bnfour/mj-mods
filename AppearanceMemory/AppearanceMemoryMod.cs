3cd35a9b3d4ba1a29a00e67ecd1d28a6d44b6d85
Bn4 <bnfour@bnfour.net>
2026-01-30 19:54:01 +0700

    a project for the first actual mod in this repo -- currently does nothing
    it's going to actually store the bg image/tray color settings because for some reason these do not work in vanilla for me, even though some code to handle that appears to exist

---
+++ AppearanceMemory/AppearanceMemoryMod.cs
@@ -0,0 +1,10 @@
+using MelonLoader;
+
+namespace Bnfour.MoeJigsawMods.AppearanceMemory;
+
+public class AppearanceMemoryMod : MelonMod
+{
+    // TODO handle prefs
+
+    // TODO patch, helper
+}
---
+++ AppearanceMemory/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using MelonLoader;
+
+using Bnfour.MoeJigsawMods.AppearanceMemory;
+
+[assembly: MelonInfo(typeof(AppearanceMemoryMod), "Appearance memory", "0.0.1", "bnfour", "https://github.com/bnfour/mj-mods")]
+[assembly: MelonGame("Ares inc.", "JigsawPuzzle")]
+[assembly: MelonColor(255, 202, 80, 16)]
+[assembly: MelonAuthorColor(255, 128, 128, 128)]
+
+[assembly: AssemblyDescription("Actually remembers the background and tray settings")]
+[assembly: AssemblyCopyright("bnfour 2026; open-source")]
