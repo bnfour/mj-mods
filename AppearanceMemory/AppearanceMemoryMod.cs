3ec26340e101a98d46132f6a299a09fd133f42ed
Bn4 <bnfour@bnfour.net>
2026-01-30 20:10:14 +0700

    appearance memory: (melon) preferences to store the selected indices

--- AppearanceMemory/AppearanceMemoryMod.cs
+++ AppearanceMemory/AppearanceMemoryMod.cs
@@ -4,7 +4,29 @@ namespace Bnfour.MoeJigsawMods.AppearanceMemory;
 
 public class AppearanceMemoryMod : MelonMod
 {
-    // TODO handle prefs
+    private MelonPreferences_Category _category;
+    private MelonPreferences_Entry<int> _skinId;
+    private MelonPreferences_Entry<int> _trayId;
 
-    // TODO patch, helper
+    // TODO constraints? 1–8 for skin, 1–5 for tray
+
+    internal int Skin
+    {
+        get => _skinId.Value;
+        set => _skinId.Value = value;
+    }
+
+    internal int Tray
+    {
+        get => _trayId.Value;
+        set => _trayId.Value = value;
+    }
+
+    public override void OnInitializeMelon()
+    {
+        // TODO descriptions
+        _category = MelonPreferences.CreateCategory("Bnfour_AppearanceMemory");
+        _skinId = _category.CreateEntry("Skin", 1);
+        _trayId = _category.CreateEntry("Tray", 1);
+    }
 }
