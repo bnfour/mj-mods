06be5a9cfa2e8b9cabc2270fc3035f14ab5654b8
Bn4 <bnfour@bnfour.net>
2026-02-06 21:31:44 +0700

    piece freeze: some data to store in the mode: a wip set of pieces to freeze (only supports a single puzzle for now, no save/load -- to be improved later) and a key state for the (un)lock modifier
    i tried reimagining the usual keydown/keyup code as a FSM (i even sketched it), the expression should work unless i'm very wrong
    
    this data being mod-global is not very optimal, but then again, this entire mod is, too (you'll see)

--- PieceFreeze/PieceFreezeMod.cs
+++ PieceFreeze/PieceFreezeMod.cs
@@ -1,8 +1,22 @@
-﻿using MelonLoader;
+﻿using System.Collections.Generic;
+using MelonLoader;
+using UnityEngine;
 
 namespace Bnfour.MoeJigsawMods.PieceFreeze;
 
 public class PieceFreezeMod : MelonMod
 {
+    // TODO replace with something that supports storing data for multiple puzzles and file save/load
+    // stores piece names as generated in vanilla, piece_XX_YY
+    internal HashSet<string> LockedPieces { get; } = new();
 
+    internal bool FreezeModifierDown { get; private set; }
+
+    // TODO make configurable?
+    private const KeyCode ModifierKey = KeyCode.LeftAlt;
+
+    public override void OnUpdate()
+    {
+        FreezeModifierDown = (FreezeModifierDown || Input.GetKeyDown(ModifierKey)) && !Input.GetKeyUp(ModifierKey);
+    }
 }
