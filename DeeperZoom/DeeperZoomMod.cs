bdff2e96c4b3cd48a912bdef1cccc27f6ad0bee4
Bn4 <bnfour@bnfour.net>
2026-02-21 00:45:20 +0700

    deeper zoom: comments for the preferences, updated version of default config section in readme

--- DeeperZoom/DeeperZoomMod.cs
+++ DeeperZoom/DeeperZoomMod.cs
@@ -21,11 +21,14 @@ public class DeeperZoomMod : MelonMod
     public override void OnInitializeMelon()
     {
         _preferencesCategory = MelonPreferences.CreateCategory("Bnfour_DeeperZoom");
-        // TODO finalize validation, esp. max values; descriptions as well
-        _maxMatScale = _preferencesCategory.CreateEntry("MaxScale", 2f, validator: new ValueRange<float>(1f, 10f));
-        _zoomSteps = _preferencesCategory.CreateEntry("ZoomSteps", 11, validator: new ValueRange<int>(2, 64));
+        _maxMatScale = _preferencesCategory.CreateEntry("MaxScale", 2f, "Maximum zoom",
+        "Maximum zoom value for the puzzle. 1--10 (float), vanilla default is 1.", validator: new ValueRange<float>(1f, 10f));
+        _zoomSteps = _preferencesCategory.CreateEntry("ZoomSteps", 11, "Zoom steps",
+        "Number of zoom level between minimum and maximum. Inclusive, so can't be less than 2 (max value is 64).", validator: new ValueRange<int>(2, 64));
         // match vanilla by default
-        _maximumPreviewZoom = _preferencesCategory.CreateEntry("PreviewZoomMax", 10, validator: new ValueRange<int>(2, 50));
-        _minimumPreviewZoom = _preferencesCategory.CreateEntry("PreviewZoomMin", 4, validator: new ValueRange<int>(1, 49));
+        _maximumPreviewZoom = _preferencesCategory.CreateEntry("PreviewZoomMax", 10, "Preview's maximum zoom",
+        "Maximum zoom value for the preview image. 2--50 (int), vanilla default is 10.", validator: new ValueRange<int>(2, 50));
+        _minimumPreviewZoom = _preferencesCategory.CreateEntry("PreviewZoomMin", 4, "Preview's maximum zoom",
+        "Maximum zoom value for the preview image. 1--49 (int), vanilla default is 4. Should be less than maximum value.", validator: new ValueRange<int>(1, 49));
     }
 }
