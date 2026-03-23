ba1596f385144894e09ac44f74f9b6582aa94437
Bn4 <bnfour@bnfour.net>
2026-03-24 02:41:23 +0700

    deeper zoom: fixed the giant jump in zoom levels on first zoom change after the puzzle is opened
    so basically, half of the internal state were not updated for the deeper zoom:
    - baseMatScale, _the_ current zoom level value, was always set to 10, as the vanilla maximum
    - the display zoom was set to 1x, the vanilla maximum
    
    (values for mod's default config) so when we first scrolled the wheel, the baseMatScale were updated by plus-minus one, and the display zoom was finally updated to something close to 2x which levels 9 or 11 represent, making the giant leap
    
    now, both baseMatScale and zoom level are updated to the level closest to 1.0x on puzzle open; baseMatScale 5 for 1.001458x zoom on mod's default config
    
    (i tried to see the difference between 1x exactly and 1.001458x, but alas)

--- DeeperZoom/Patches/JigsawMainMakeMainFramePatch.cs
+++ DeeperZoom/Patches/JigsawMainMakeMainFramePatch.cs
@@ -1,18 +1,38 @@
+using System.Linq;
 using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
 
 using Jigsaw;
-using MelonLoader;
 
 namespace Bnfour.MoeJigsawMods.DeeperZoom.Patches;
 
 /// <summary>
 /// Replaces the maximum zoom scale with a custom one.
+/// Changes the zoom level on puzzle open to the closest one to 1.0x, the vanilla value.
 /// </summary>
 [HarmonyPatch(typeof(JigsawMain), "MakeMainFrame")]
 public class JigsawMainMakeMainFramePatch
 {
-    internal static void Postfix(ref float ___matScaleMax)
+    internal static void Postfix(ref float ___matScaleMax, ref int ___baseMatScale,
+        float ___matScaleMin, JigsawMain __instance)
     {
-        ___matScaleMax = Melon<DeeperZoomMod>.Instance.MatMaxScale;
+        var mod = Melon<DeeperZoomMod>.Instance;
+
+        ___matScaleMax = mod.MatMaxScale;
+        // workaround for CS1628, something like "can't use a ref var in lambdas"
+        var matScaleMax = ___matScaleMax;
+
+        ___baseMatScale = Enumerable.Range(1, mod.ZoomSteps)
+            // calculate the zoom scale via lerp just like vanilla code
+            .Select(x => new LemonTuple<int, float>(x, Mathf.Abs(1f - Mathf.Lerp(___matScaleMin, matScaleMax, (float)(x - 1) / (mod.ZoomSteps - 1)))))
+            // and find the level that provides closest possible zoom to vanilla's default of 1.0 (also the vanilla maximum)
+            .OrderBy(t => t.Item2)
+            .First().Item1;
+
+        // set zoom to the level we found as default
+        var traverse = Traverse.Create(__instance);
+        traverse.Method("CalcMovableArea").GetValue();
+        traverse.Method("AdjustMatPosition").GetValue();
     }
 }
