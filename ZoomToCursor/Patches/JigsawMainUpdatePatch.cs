6ae6bc0458f06e54ac4cd6db027ae9a1c2990fd9
Bn4 <bnfour@bnfour.net>
2026-04-13 15:05:05 +0700

    zoom to cursor: fully implemented

--- ZoomToCursor/Patches/JigsawMainUpdatePatch.cs
+++ ZoomToCursor/Patches/JigsawMainUpdatePatch.cs
@@ -11,18 +11,18 @@ namespace Bnfour.MoeJigsawMods.ZoomToCursor.Patches;
 [HarmonyPatch(typeof(JigsawMain), "Update")]
 internal class JigsawMainUpdatePatch
 {
+    // it's more of a reused call than a method, so i didn't move it into a helper call as usual
+    private static Vector2 MousePositionInObjectCoords(GameObject go)
+        => (Vector2)go.transform.InverseTransformPoint(Camera.main.ScreenToWorldPoint(InputCapture.GetCursorWorldPos()));
+
     internal static void Prefix(int ___baseMatScale, GameObject ___goMatBase,
         out ZoomState __state)
     {
         // store the state before the update
-
-        var currentPositionOverObject = ___goMatBase.transform.InverseTransformPoint(
-            Camera.main.ScreenToWorldPoint(InputCapture.GetCursorWorldPos()));
-
         __state = new()
         {
             ZoomLevel = ___baseMatScale,
-            PositionOverTheObject = (Vector2)currentPositionOverObject
+            PositionOverTheObject = MousePositionInObjectCoords(___goMatBase)
         };
     }
 
@@ -32,7 +32,10 @@ internal class JigsawMainUpdatePatch
     {
         if (___baseMatScale != __state.ZoomLevel)
         {
-            // TODO stuff
+            ___vCamPos += MousePositionInObjectCoords(___goMatBase) - __state.PositionOverTheObject;
+
+            // apply camera position change
+            Traverse.Create(__instance).Method("AdjustMatPosition").GetValue();
         }
     }
 }
