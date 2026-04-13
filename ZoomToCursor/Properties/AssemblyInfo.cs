994de73c248d72ec67092ff83a5b4a13463edcab
Bn4 <bnfour@bnfour.net>
2026-04-13 15:18:57 +0700

    zoom to cursor: version bumped to 1.0.0 before i forget

--- ZoomToCursor/Properties/AssemblyInfo.cs
+++ ZoomToCursor/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using MelonLoader;
 
 using Bnfour.MoeJigsawMods.ZoomToCursor;
 
-[assembly: MelonInfo(typeof(ZoomToCursorMod), "Zoom to cursor", "0.0.1", "bnfour", "https://github.com/bnfour/mj-mods")]
+[assembly: MelonInfo(typeof(ZoomToCursorMod), "Zoom to cursor", "1.0.0", "bnfour", "https://github.com/bnfour/mj-mods")]
 [assembly: MelonGame("Ares inc.", "JigsawPuzzle")]
 [assembly: MelonColor(255, 202, 80, 16)]
 [assembly: MelonAuthorColor(255, 128, 128, 128)]
